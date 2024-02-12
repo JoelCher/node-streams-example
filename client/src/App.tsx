7dde307b12e49d4330d6fd7451e993c2f8c759bf
JoelCher <joelcher@hilma.tech>
2024-02-12 14:51:25 +0200

    "init'

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [progress, setProgress] = useState(0)
+  const [progress, setProgress] = useState<number | null>(null)
   const [users, setUsers] = useState([])
   useEffect(() => {
     fetch('http://localhost:8080/stream', {
@@ -17,8 +17,6 @@ function App() {
       function read() {
         reader.read().then(({ done, value }) => {
           if (done) {
-            console.log('Stream complete');
-            console.log('end data is ', endData)
             //now set the query data with react query or use a state or whatever
             setUsers(endData)
             return;
@@ -26,9 +24,9 @@ function App() {
           const enc = new TextDecoder()
           //data is either a progress percentage or the final data
           const data = JSON.parse(enc.decode(value))
-          setProgress(data)
+          if (typeof data === 'number')
+            setProgress(data)
           endData = data
-          console.log(data)
           // Continue reading
           read();
         });
@@ -36,19 +34,18 @@ function App() {
       read();
     })
   }, [])
+
   return (
     <div className='home-page'>
-      <h1>Progress is {typeof progress === 'number' && progress}</h1>
+      <h1>Progress is {progress}</h1>
       {
         users.length && users.map((us: { username: string }, index) => {
           const username = us.username
           return (
-            <div key={index}>{username ?? ""}</div>
+            <div key={index}>{username}</div>
           )
         })
       }
-
-
     </div>
   )
 }
