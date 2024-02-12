ba433e807b13269dbcc31d616e374c795c8b9624
JoelCher <joelcher@hilma.tech>
2024-02-12 15:01:41 +0200

    remove app service

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,22 +1,16 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
-import { AppService } from './app.service';
 import { Response, Request } from 'express';
 import { PassThrough } from 'stream';
 import { faker } from '@faker-js/faker'
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor() {}
 
 
   @Get('stream')
   streamData(@Res() res: Response, @Req() req: Request) {
     let index = 0;
-    const pipe = new PassThrough();
+    const stream = new PassThrough();
     const numOfUsers = 10;
     const users = [];
     for (let i = 0; i < 10; i++) {
@@ -24,21 +18,21 @@ export class AppController {
         username: faker.person.firstName(),
       });
     }
-    pipe.pipe(res);
+    stream.pipe(res);
     const intervalId = setInterval(async () => {
       if (index > 8) {
-        res.write(Buffer.from(JSON.stringify(users)))
-        res.end()
-        clearInterval(intervalId)
-        return
+        res.write(Buffer.from(JSON.stringify(users)));
+        res.end();
+        clearInterval(intervalId);
+        return;
       }
       index++;
-      pipe.write(Buffer.from(JSON.stringify((index / numOfUsers) * 100)))
+      stream.write(Buffer.from(JSON.stringify((index / numOfUsers) * 100)));
     }, 1000);
 
     req.on('close', () => {
       clearInterval(intervalId);
-      res.end()
+      res.end();
     });
   }
 }
